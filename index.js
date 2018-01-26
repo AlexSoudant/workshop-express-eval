282108f6bf95852638190775675ee59214280fb9
Alex Soudant <soudant.alex@gmail.com>
2018-01-26 15:15:25 +0100

    soluce to exercice 2

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-let chalk = require('chalk');
-
 const app = require('./src/app');
 
 // port
-const port = 3000;
+const port = process.argv[2]|| 3000;
 
 // start server here
-// console.log(chalk.green('Hello web server'));
-
 app.get('/', function (req, res) {
   res.send('Hello web server')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 })
